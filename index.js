e555491e6d35f1f354b9bd4b3b0dca476910e40f
orgil-12 <theorgilbold@gmail.com>
2025-01-07 19:25:50 +0800

    init new code:

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const fs = require("node:fs");
 const app = express();
 const port = 3000;
 
+app.use(express.json());
+
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
@@ -14,16 +16,13 @@ app.get("/movies", (req, res) => {
   res.json(movies);
 });
 
-app.get("/movies/create", (req, res) => {
-  const { query } = req;
-  //1. read json from file
+app.post("/movies", (req, res) => {
+  const { body } = req;
+  console.log(body)
   const data = fs.readFileSync("data/movies.json", "utf8");
   const movies = JSON.parse(data);
 
-  //2. push to json array
-  movies.push({ id: Date.now(), name: query.name });
-
-  //3. write json to string
+  movies.push({ id: Date.now(), ...body });
 
   const moviesString = JSON.stringify(movies, null, 4);
   fs.writeFileSync("data/movies.json", moviesString);
@@ -31,25 +30,30 @@ app.get("/movies/create", (req, res) => {
   res.json({ message: "Success" });
 });
 
-app.get("/movies/update", (req, res) => {
-  const { query } = req;
+app.put("/movies/:id", (req, res) => {
+  const {body,params} = req
+
   const data = fs.readFileSync("data/movies.json", "utf-8");
   const movies = JSON.parse(data);
-  const movieIndex = movies.findIndex((movie) => movie.id === Number(query.id));
-  if (movieIndex !== -1) {
-    movies[movieIndex] = { id: Number(query.id), name: query.updateName };
+
+  const movieIndex = movies.findIndex((movie) => movie.id === Number(params.id));
+
+  if (movieIndex >= 0) {
+    movies[movieIndex] = { id: Number(params.id), ...movies[movieIndex],  ...body };
   } else return res.status(400).json({ error: "Cannot find this id" });
+
   const moviesString = JSON.stringify(movies, null, 4);
   fs.writeFileSync("data/movies.json", moviesString);
+
   res.json({ message: "Updated" });
 });
 
-app.get("/movies/delete", (req, res) => {
-  const { query } = req;
+app.delete("/movies/:id", (req, res) => {
+  const { params } = req;
   const data = fs.readFileSync("data/movies.json", "utf-8");
   const movies = JSON.parse(data);
-  const movieIndex = movies.findIndex((movie) => movie.id === Number(query.id));
-  if (movieIndex !== -1) {
+  const movieIndex = movies.findIndex((movie) => movie.id === Number(params.id));
+  if (movieIndex >= 0) {
     movies.splice(movieIndex, 1);
   } else return res.status(400).json({ error: "Cannot find this id" });
   const moviesString = JSON.stringify(movies, null, 4);
@@ -57,11 +61,11 @@ app.get("/movies/delete", (req, res) => {
   res.json({ message: "Deleted" });
 });
 
-app.get("/movies/detail", (req, res) => {
-  const { query } = req;
+app.get("/movies/:id", (req, res) => {
+  const { params } = req;
   const data = fs.readFileSync("data/movies.json", "utf-8");
   const movies = JSON.parse(data);
-  const movie = movies.find((movie) => movie.id === Number(query.id));
+  const movie = movies.find((movie) => movie.id === Number(params.id));
   if (movie) {
     res.json(movie);
   } else return res.status(400).json({ error: "Cannot find this id" });
